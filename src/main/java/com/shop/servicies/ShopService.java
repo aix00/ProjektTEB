9f01947f1fcf3d2e612dccb281adedbe72683c7d
Riukado <tjeskestock@gmail.com>
2022-03-12 09:24:08 +0100

    Added methods to ShopService to allow for: Reading, Updating and Deleting products from repository.

--- src/main/java/com/shop/servicies/ShopService.java
+++ src/main/java/com/shop/servicies/ShopService.java
@@ -1,6 +1,8 @@
 package com.shop.servicies;
 
 import com.shop.controllers.HttpShop;
+import com.shop.exceptions.NotFoundException;
+import com.shop.repositories.ShopEntity;
 import com.shop.repositories.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,18 @@ public class ShopService {
     public String createShopProduct(HttpShop httpShop) {
         Shop shop = new Shop(httpShop.getProductName(), httpShop.getCompanyName(), httpShop.getPrice());
         return shopRepository.createShopProduct(shop);
+    }
+    public Iterable<ShopEntity> getShopProduct(){
+        return shopRepository.getShopProduct();
+    }
+    public ShopEntity getSpecifiedShopProduct(Integer id) throws NotFoundException {
+        return shopRepository.getShopSpecifiedProduct(id);
+    }
+    public void updateProduct(Integer id, HttpShop httpShop) throws NotFoundException{
+        Shop shop = new Shop(httpShop.getProductName(),httpShop.getCompanyName(),httpShop.getPrice());
+        shopRepository.updateProduct(id, shop);
+    }
+    public void deleteProduct(Integer id){
+        shopRepository.deleteProduct(id);
     }
 }
