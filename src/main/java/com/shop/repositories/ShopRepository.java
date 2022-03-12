e63a51977e0a89e3cc5825a35a84d3f798b4e73b
Riukado <tjeskestock@gmail.com>
2022-03-12 08:57:43 +0100

    Fixed repository error from last week. Added option to get all products and get a specific product using ID.

--- src/main/java/com/shop/repositories/ShopRepository.java
+++ src/main/java/com/shop/repositories/ShopRepository.java
@@ -4,6 +4,7 @@ import com.shop.servicies.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -13,10 +14,23 @@ public class ShopRepository {
     @Autowired
     private ShopDatabaseRepository shopDatabaseRepository;
 
+    //Adding a product
     public String createShopProduct(Shop shop) {
         String id = UUID.randomUUID().toString();
         ShopEntity shopEntity = new ShopEntity(shop.getProductName(),shop.getCompanyName(),shop.getPrice());
-        ShopEntity savedEntity = shopDatabaseRepository.save(ShopEntity);
+        ShopEntity savedEntity = shopDatabaseRepository.save(shopEntity);
         return String.valueOf(savedEntity.getId());
     }
+
+    //Getting all products from ShopEntity
+    public Iterable<ShopEntity> getShopProduct(){
+        return shopDatabaseRepository.findAll();
+    }
+    //Getting a specific product from ShopEntity based on its ID
+    public Optional<ShopEntity> getShopSpecifiedProduct(long id){
+        return shopDatabaseRepository.findById(id);
+    }
+
+
+
 }
